d151f2c02f08fa59b1c4d5b2a3a87585929fb741
WenmuZhou <572459439@qq.com>
2021-10-27 13:54:21 +0000

    add ips to PSENET

--- models/post_processing/pse/__init__.py
+++ models/post_processing/pse/__init__.py
@@ -1,1 +1,15 @@
+import sys
+import os
+import subprocess
+
+python_path = sys.executable
+
+ori_path = os.getcwd()
+os.chdir('models/post_processing/pse')
+if subprocess.call(
+        '{} setup.py build_ext --inplace'.format(python_path), shell=True) != 0:
+    raise RuntimeError('Cannot compile pse: {}'.format(
+        os.path.dirname(os.path.realpath(__file__))))
+os.chdir(ori_path)
+
 from .pse import pse
